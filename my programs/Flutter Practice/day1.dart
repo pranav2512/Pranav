b3e13746b5508d706f39e71e08aec760c9289305
Pranav <83396279+pranav2512@users.noreply.github.com>
2023-08-10 18:25:14 +0530

    Add files via upload

---
+++ my programs/Flutter Practice/day1.dart
@@ -0,0 +1,44 @@
+void main()
+{
+  //demo
+  /*red and 
+  white*/
+  ///students
+
+  print("hello");
+
+  int a=10;
+  double d=10.5;
+  String s="raj";
+  bool b=true;
+
+  print(a);
+  print(d);
+  print(s);
+  print(b);
+
+  dynamic d1="pranav";
+
+  print(d1);
+
+  var string1="neel";
+  print(string1);
+
+  const int c1=15000;
+  print(c1);
+
+  final String f1;
+  f1="chintu";
+  print(f1);
+  print(f1.length);
+
+  String? string2;
+  // ignore: dead_code
+  print(string2?.length??0);
+
+  int num1=10,num2=20;
+  print("The addition of $num1 and $num2 is = ${num1+num2}");
+
+  String string3="Harsh and Manav";
+  print("The lenght of string3 is = ${string3.length}");
+  }
